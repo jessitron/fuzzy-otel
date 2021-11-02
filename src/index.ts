f456f56702db59730580216bc776b8c7b4b412ca
Jessica Kerr <jessitron@gmail.com>
2021-11-02 19:59:25 +0000

    Basic trace to console

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
 console.log("Hello");
+
+import { BasicTracerProvider, ConsoleSpanExporter, SimpleSpanProcessor } from '@opentelemetry/sdk-trace-base';
+
+const provider = new BasicTracerProvider();
+
+// Configure span processor to send spans to the exporter
+provider.addSpanProcessor(new SimpleSpanProcessor(new ConsoleSpanExporter()));
+
+provider.register();
+
+import opentelemetry from '@opentelemetry/api';
+
+const tracer = opentelemetry.trace.getTracer('name-of-tracer');
+
+const hubertTheSpan = tracer.startSpan("hubert");
+
+hubertTheSpan.setAttribute("purpose", "What does this do?");
+
+setTimeout(() => hubertTheSpan.end(),1000 );
+
